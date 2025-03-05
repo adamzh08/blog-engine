7bc3e678911505a27690b70690cfc831b0a3846c
Adam Zhakenov <adam.zhakenov@gmail.com>
2025-03-05 03:54:43 +0100

    Add blog functionality with dynamic routing and 404 page

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { notFound } from 'next/navigation'
+import { getBlogPosts, markdownToHtml } from '../utils'
+
+export default async function Page({ params }: {
+  params: { slug: string }
+}) {
+  const { slug } = await params
+
+  const posts = getBlogPosts()
+  const post = posts.find((post) => post.slug === slug)
+
+  if (!post) {
+    notFound()
+  }
+
+  const content = await markdownToHtml(post.content)
+
+  return (
+    <article className="prose dark:prose-invert">
+      <h1>{post.metadata.title}</h1>
+      <div 
+        dangerouslySetInnerHTML={{ __html: content }} 
+      />
+    </article>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Mono } from "next/font/google";
+import { Navbar } from './components/nav'
+import Footer from './components/footer'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const main_font = Space_Mono({
+  variable: "--font-main",
+  weight: "400",
+  style: "normal",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${main_font.variable} antialiased max-w-xl mx-4 mt-8 lg:mx-auto`}>
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 font-[family-name:var(--font-main)]">
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
